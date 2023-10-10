6cdc69b2d791059b38165cc984b1fb4de5164874
Valeriy Lvov <tepliy_shov@mail.ru>
2023-10-10 22:24:29 +0500

    Добавил в функцию upload_epic_nasa_photo вызов функции upload_image

--- fetch_nasa_epic_images.py
+++ fetch_nasa_epic_images.py
@@ -1,5 +1,4 @@
 import argparse
-import os
 import requests
 from space_photos import upload_image
 
@@ -15,17 +14,15 @@ def upload_epic_nasa_photo(token, upload_to):
     response = requests.get(url)
     response.raise_for_status()
     content = response.json()
-    original_content = [(entry['date'], entry['image']) for entry in content if 'date' in entry and 'image' in entry]
+    original_links = [(entry['date'], entry['image']) for entry in content if 'date' in entry and 'image' in entry]
 
-    for url_number, (date, image) in enumerate(original_content):
+    for url_number, (date, image) in enumerate(original_links):
         year = date[0:4]
         month = date[5:7]
         day = date[8:10]
         image_url = f'https://api.nasa.gov/EPIC/archive/natural/{year}/{month}/{day}/png/{image}.png'
         image_response = requests.get(image_url, headers=headers, params=payload)
-        filename = os.path.join(upload_to, f'epic_photo_{url_number}.png')
-        with open(filename, 'wb') as file:
-            file.write(image_response.content)
+        upload_image(image_response.url, upload_to, f'epic_photo_{url_number}.png')
 
 
 if __name__ == '__main__':
